af86a19c7521f70a66a8dc6d65db470aa7f92b2e
Prateek Chandra <prateekchandra1027@gmail.com>
2024-09-29 03:35:33 +0900

    Update app.js for app

--- api/app.js
+++ api/app.js
@@ -13,7 +13,7 @@ import http from "http";
 
 const app = express();
 
-app.use(cors({origin: "http://localhost:5173", credentials:true }));
+app.use(cors({origin: "https://realestate-frontend-2.onrender.com", credentials:true }));
 app.use(express.json());
 app.use(cookieParser());
 
