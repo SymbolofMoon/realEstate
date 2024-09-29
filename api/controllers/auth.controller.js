2f86fb52e3d621de257f0feee1ec98582f9ee37c
Prateek Chandra <prateekchandra1027@gmail.com>
2024-09-29 13:44:55 +0900

    Update auth.controller.js

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -78,6 +78,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
             httpOnly: true,
             secure: true,
+            sameSite: 'None',
             maxAge: age,
         })
         .status(200)
